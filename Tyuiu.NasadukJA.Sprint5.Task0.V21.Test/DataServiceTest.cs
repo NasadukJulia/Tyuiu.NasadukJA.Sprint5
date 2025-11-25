7ae9d60d1099456b6b8f6851b36e492a27ef29c3
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-11-25 23:01:56 +0500

    Add project files.

---
+++ Tyuiu.NasadukJA.Sprint5.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+using Tyuiu.NasadukJA.Sprint5.Task0.V21.Lib;
+
+namespace Tyuiu.NasadukJA.Sprint5.Task0.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Julia\source\repos\Tyuiu.NasadukJA.Sprint5\Tyuiu.NasadukJA.Sprint5.Task0.V21\bin\Debug\net8.0\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.NasadukJA.Sprint5.Task0.V21/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.NasadukJA.Sprint5.Task0.V21.Lib;
+
+// See https://aka.ms/new-console-template for more information
+
+int x = 3;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #21                                                             *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+Console.WriteLine("* стекстовый файл OutPutFileTask0.txt и вывести на консоль.               *");
+Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("x = " + x);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(x);
+
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
