fbd567ac45662cdcd93c75a71a326852b873f304
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-11-26 21:11:47 +0500

    Task2

--- Tyuiu.NasadukJA.Sprint5.Task2.V6.Lib/DataService.cs
+++ Tyuiu.NasadukJA.Sprint5.Task2.V6.Lib/DataService.cs
@@ -1,7 +1,63 @@
-﻿namespace Tyuiu.NasadukJA.Sprint5.Task2.V6.Lib
-{
-    public class Class1
-    {
+﻿using System.IO;
 
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NasadukJA.Sprint5.Task2.V6.Lib
+{
+    public class DataService : ISprint5Task2V6
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] > 0) matrix[i, j] = 1;
+                    else matrix[i, j] = 0;
+                }
+            }
+
+            string str = "";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str += matrix[i, j] + ";";
+                    }
+                    else
+                    {
+                        str += matrix[i, j];
+                    }
+                }
+
+                if (i != rows - 1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+            }
+
+            return path;
+        }
     }
 }
+
--- Tyuiu.NasadukJA.Sprint5.Task2.V6/Program.cs
+++ Tyuiu.NasadukJA.Sprint5.Task2.V6/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.NasadukJA.Sprint5.Task2.V6.Lib;
+
+// See https://aka.ms/new-console-template for more information
+
+int[,] mtrx = new int[3, 3] { { -2, 1, 8 },
+                              {-4, -7, 8 },
+                              {6, 5, 5 } };
+
+int rows = mtrx.GetUpperBound(0) + 1;
+int columns = mtrx.Length / rows;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #6                                                              *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+Console.WriteLine("* значениями с клавиатуры. Заменить + элементы массива на 1, а - на 0.    *");
+Console.WriteLine("* Сохранить результат в OutPutFileTask2.txt                               *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(mtrx);
+
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
