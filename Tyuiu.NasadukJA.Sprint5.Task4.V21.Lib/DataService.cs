8ef586ae218b702484d59fbcaaf41b32dc92b9be
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-11-27 18:04:54 +0500

    Task4

--- Tyuiu.NasadukJA.Sprint5.Task4.V21.Lib/DataService.cs
+++ Tyuiu.NasadukJA.Sprint5.Task4.V21.Lib/DataService.cs
@@ -1,7 +1,20 @@
-﻿namespace Tyuiu.NasadukJA.Sprint5.Task4.V21.Lib
-{
-    public class Class1
-    {
+﻿using System.IO;
 
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NasadukJA.Sprint5.Task4.V21.Lib
+{
+    public class DataService : ISprint5Task4V21
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double x = double.Parse(strX, System.Globalization.CultureInfo.InvariantCulture);
+
+            double y = x * x * x * Math.Cos(x) + 2 * x;
+            y = Math.Round(y, 3);
+
+            return y;
+        }
     }
 }
--- Tyuiu.NasadukJA.Sprint5.Task4.V21/Program.cs
+++ Tyuiu.NasadukJA.Sprint5.Task4.V21/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.NasadukJA.Sprint5.Task4.V21.Lib;
+
+// See https://aka.ms/new-console-template for more information
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #21                                                             *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле             *");
+Console.WriteLine("* Вычислить значение по формуле и вернуть результат на консоль.           *");
+Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask4V21.txt");
+
+Console.WriteLine("Данные находятся в файле: " + path);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double res = ds.LoadFromDataFile(path);
+
+Console.WriteLine(res);
+Console.ReadKey();
