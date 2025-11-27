2005fa9a009edcbe51509b4a8647ce56dfecf985
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-11-27 21:24:39 +0500

    Task7

--- Tyuiu.NasadukJA.Sprint5.Task7.V4.Lib/DataService.cs
+++ Tyuiu.NasadukJA.Sprint5.Task7.V4.Lib/DataService.cs
@@ -1,7 +1,56 @@
-﻿namespace Tyuiu.NasadukJA.Sprint5.Task7.V4.Lib
-{
-    public class Class1
-    {
+﻿using System.IO;
 
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NasadukJA.Sprint5.Task7.V4.Lib
+{
+    public class DataService : ISprint5Task7V4
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V4.txt");
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char c = line[i];
+
+                        if ((c >= 'А' && c <= 'я') || c == 'ё' || c == 'Ё')
+                        {
+                            strLine += "#";
+                        }
+                        else
+                        {
+                            strLine += c;
+                        }
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+
+            }
+
+            return pathSaveFile;
+        }
     }
 }
+
+
+        
+
+        
--- Tyuiu.NasadukJA.Sprint5.Task7.V4.Test/DataServiceTest.cs
+++ Tyuiu.NasadukJA.Sprint5.Task7.V4.Test/DataServiceTest.cs
@@ -1,11 +1,22 @@
-﻿namespace Tyuiu.NasadukJA.Sprint5.Task7.V4.Test
+﻿using System.IO;
+
+using Tyuiu.NasadukJA.Sprint5.Task7.V4.Lib;
+
+namespace Tyuiu.NasadukJA.Sprint5.Task7.V4.Test
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckedExistsFile()
         {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V4.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
